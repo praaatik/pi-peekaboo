7f9b21a8f1b0e3ba4767c8c0ac71e37d3aaae394
Pratik Kulkarni <pratikkulkarni1307@gmail.com>
2020-10-05 08:45:53 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React, { useState } from "react";
+import "./App.css";
+import github from "./github-icon.svg";
+
+const url = "https://uploadbeta.com/api/pi/?cached&n=1000000";
+var piDigits = "";
+var digitsBefore = ""; //the digits before the ones found in Pi
+var digitsAfter = ""; //the digits after the ones found in Pi
+var output = ""; //the output which is to be displayed
+var n = NaN;
+
+// Fetching the Pi digits from the API above.
+// Referred from https://helloacm.com/pi/
+fetch(url)
+  .then((response) => response.json())
+  .then((data) => (piDigits = data.substring(1)));
+
+function App() {
+  const [input, setInput] = useState("");
+
+  function findInPi(input) {
+    //Searching for the input digits in PiDigits
+    //If found, the value entered is stored in n, else -1 is returned by indexOf
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String/indexOf
+
+    n = piDigits.indexOf(input);
+    digitsAfter = "";
+    digitsBefore = "";
+
+    if (n !== -1) {
+      //Condition true if the digits are found in the firs 1 Million digits of Pi
+      //Considering the 20 digits before and after Pi, just for frontend
+      for (var i = 1; i <= 25; i++) {
+        digitsBefore += piDigits.charAt(n - i);
+        digitsAfter += piDigits.charAt(n + i + input.length - 1);
+      }
+      digitsBefore = digitsBefore.split("").reverse("").join("");
+      output = "..." + digitsBefore + " " + input + " " + digitsAfter + "...";
+      console.log(input);
+      // console.log(output);
+      return output;
+    } else {
+      return "";
+    }
+  }
+
+  return (
+    <div className="App">
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => {
+          findInPi(e.target.value);
+          setInput(e.target.value);
+        }}
+        className="input"
+        placeholder="Enter string"
+        name="input"
+      />
+
+      <div className="output">
+        {output === "" ? (
+          <p>Not Found!</p>
+        ) : (
+          <div>
+            {n === -1 ? (
+              <p>
+                This string does not occur in the first 1 Million digits of Pi!
+              </p>
+            ) : (
+              <p>
+                The string occurs at location{" "}
+                <span style={{ color: " #222f3e" }}>{n}</span>
+              </p>
+            )}
+
+            <p>
+              {"..." + digitsBefore}{" "}
+              <span style={{ color: " #222f3e" }}>{input}</span>{" "}
+              {digitsAfter + "..."}
+            </p>
+          </div>
+        )}
+      </div>
+      <div className="footer">
+        <a href="https://www.github.com/praaatik">
+          <img src={github} width="30px" height="30px" className="github-icon"/>
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default App;
